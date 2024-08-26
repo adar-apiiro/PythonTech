ca8fbbe15f7d13d5d934332518a83c786756cf67
Adar Azulay <adar@apiiro.com>
2024-08-26 11:07:45 +0300

    fix

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import snowflake.connector
-conn = snowflake.connector.connect(
-    user='XXXX',
-    password='XXXX',
-    account='XXXX',
-    session_parameters={
-      "CLIENT_TELEMETRY_ENABLED": False,
-    }
-)
+import snowflake
+def hello():
+    # Create a connection object
+    conn = snowflake.connector.connect(
+        user='XXXX',
+        password='XXXX',
+        account='XXXX',
+        session_parameters={
+            "CLIENT_TELEMETRY_ENABLED": False,
+        }
+    )
+    conn.close()
+
+if __name__ == '__main__':
+    hello()
